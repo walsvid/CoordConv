3f8626458fe97123d7a15598506a6f7396b80094
walsvid <walsvid@gmail.com>
2018-07-17 22:56:01 +0800

    simplify addcoord function

--- coordconv.py
+++ coordconv.py
@@ -10,15 +10,14 @@ class AddCoords(nn.Module):
         self.with_r = with_r
 
     def forward(self, input_tensor):
-        r"""
-        input_tensor: (N, C_in,H,W)
-        :param input_tensor:
+        """
+        :param input_tensor: shape (N, C_in, H, W)
         :return:
         """
         if self.rank == 1:
             batch_size_shape, channel_in_shape, dim_x = input_tensor.shape
-            xx_range = torch.arange(dim_x, dtype=torch.int32).unsqueeze(0)
-            xx_channel = xx_range.unsqueeze(0)
+            xx_range = torch.arange(dim_x, dtype=torch.int32)
+            xx_channel = xx_range[None, None, :]
 
             xx_channel = xx_channel.float() / (dim_x - 1)
             xx_channel = xx_channel * 2 - 1
@@ -32,20 +31,19 @@ class AddCoords(nn.Module):
             if self.with_r:
                 rr = torch.sqrt(torch.pow(xx_channel - 0.5, 2))
                 out = torch.cat([out, rr], dim=1)
+
         elif self.rank == 2:
             batch_size_shape, channel_in_shape, dim_y, dim_x = input_tensor.shape
-            xx_ones = torch.ones([1, dim_x], dtype=torch.int32)
-            yy_ones = torch.ones([1, dim_y], dtype=torch.int32)
+            xx_ones = torch.ones([1, 1, 1, dim_x], dtype=torch.int32)
+            yy_ones = torch.ones([1, 1, 1, dim_y], dtype=torch.int32)
 
-            xx_range = torch.arange(dim_y, dtype=torch.int32).unsqueeze(0)
-            yy_range = torch.arange(dim_x, dtype=torch.int32).unsqueeze(0)
-            xx_range = xx_range.unsqueeze(-1)
-            yy_range = yy_range.unsqueeze(-1)
+            xx_range = torch.arange(dim_y, dtype=torch.int32)
+            yy_range = torch.arange(dim_x, dtype=torch.int32)
+            xx_range = xx_range[None, None, :, None]
+            yy_range = yy_range[None, None, :, None]
 
             xx_channel = torch.matmul(xx_range, xx_ones)
             yy_channel = torch.matmul(yy_range, yy_ones)
-            xx_channel = xx_channel.unsqueeze(0)
-            yy_channel = yy_channel.unsqueeze(0)
 
             # transpose y
             yy_channel = yy_channel.permute(0, 1, 3, 2)
@@ -68,33 +66,32 @@ class AddCoords(nn.Module):
             if self.with_r:
                 rr = torch.sqrt(torch.pow(xx_channel - 0.5, 2) + torch.pow(yy_channel - 0.5, 2))
                 out = torch.cat([out, rr], dim=1)
+
         elif self.rank == 3:
             batch_size_shape, channel_in_shape, dim_z, dim_y, dim_x = input_tensor.shape
-            xx_ones = torch.ones([1, dim_x], dtype=torch.int32)
-            yy_ones = torch.ones([1, dim_y], dtype=torch.int32)
-            zz_ones = torch.ones([1, dim_z], dtype=torch.int32)
+            xx_ones = torch.ones([1, 1, 1, 1, dim_x], dtype=torch.int32)
+            yy_ones = torch.ones([1, 1, 1, 1, dim_y], dtype=torch.int32)
+            zz_ones = torch.ones([1, 1, 1, 1, dim_z], dtype=torch.int32)
 
-            xy_range = torch.arange(dim_y, dtype=torch.int32).unsqueeze(0)
-            xy_range = xy_range.unsqueeze(-1)
+            xy_range = torch.arange(dim_y, dtype=torch.int32)
+            xy_range = xy_range[None, None, None, :, None]
 
-            yz_range = torch.arange(dim_z, dtype=torch.int32).unsqueeze(0)
-            yz_range = yz_range.unsqueeze(-1)
+            yz_range = torch.arange(dim_z, dtype=torch.int32)
+            yz_range = yz_range[None, None, None, :, None]
 
-            zx_range = torch.arange(dim_x, dtype=torch.int32).unsqueeze(0)
-            zx_range = zx_range.unsqueeze(-1)
+            zx_range = torch.arange(dim_x, dtype=torch.int32)
+            zx_range = zx_range[None, None, None, :, None]
 
             xy_channel = torch.matmul(xy_range, xx_ones)
-            xy_channel = xy_channel.unsqueeze(0)
-            xx_channel = torch.stack([xy_channel + i for i in range(dim_z)], dim=2)
+            xx_channel = torch.cat([xy_channel + i for i in range(dim_z)], dim=2)
 
             yz_channel = torch.matmul(yz_range, yy_ones)
-            yz_channel = yz_channel.unsqueeze(0)
-            yy_channel = torch.stack([yz_channel + i for i in range(dim_x)], dim=4)
+            yz_channel = yz_channel.permute(0, 1, 3, 4, 2)
+            yy_channel = torch.cat([yz_channel + i for i in range(dim_x)], dim=4)
 
             zx_channel = torch.matmul(zx_range, zz_ones)
-            zx_channel = zx_channel.unsqueeze(0)
-            zx_channel = zx_channel.permute(0, 1, 3, 2)
-            zz_channel = torch.stack([zx_channel + i for i in range(dim_y)], dim=3)
+            zx_channel = zx_channel.permute(0, 1, 4, 2, 3)
+            zz_channel = torch.cat([zx_channel + i for i in range(dim_y)], dim=3)
 
             if torch.cuda.is_available:
                 input_tensor = input_tensor.cuda()
@@ -104,8 +101,9 @@ class AddCoords(nn.Module):
             out = torch.cat([input_tensor, xx_channel, yy_channel, zz_channel], dim=1)
 
             if self.with_r:
-                rr = torch.sqrt(
-                    torch.pow(xx_channel - 0.5, 2) + torch.pow(yy_channel - 0.5, 2) + torch.pow(zz_channel - 0.5, 2))
+                rr = torch.sqrt(torch.pow(xx_channel - 0.5, 2) +
+                                torch.pow(yy_channel - 0.5, 2) +
+                                torch.pow(zz_channel - 0.5, 2))
                 out = torch.cat([out, rr], dim=1)
         else:
             raise NotImplementedError
